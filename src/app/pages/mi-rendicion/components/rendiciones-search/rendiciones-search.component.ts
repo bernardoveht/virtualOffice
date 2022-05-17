cf6e61075f2c11d8c8e2affc8093231afdb8499d
Diego Pastoriza <dpastoriza@mobilecomputing.com.ar>
2022-05-17 18:38:36 -0300

    code update

---
+++ src/app/pages/mi-rendicion/components/rendiciones-search/rendiciones-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rendiciones-search',
+  templateUrl: './rendiciones-search.component.html',
+  styleUrls: ['./rendiciones-search.component.scss']
+})
+export class RendicionesSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
