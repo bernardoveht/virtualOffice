896347b6ce67a2a3303903da965bd6a660c8462b
Bernardo Veht <bveht@lizardcode.com.ar>
2022-05-18 02:12:40 -0300

    mi gestion

--- src/app/pages/mi-gestion/mi-gestion.module.ts
+++ src/app/pages/mi-gestion/mi-gestion.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MiGestionRoutingModule } from './mi-gestion.routing.module';
+import { MiGestionComponent } from './mi-gestion.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [MiGestionComponent],
   imports: [
     CommonModule,MiGestionRoutingModule,SharedModule, ReactiveFormsModule, FormsModule
   ]
