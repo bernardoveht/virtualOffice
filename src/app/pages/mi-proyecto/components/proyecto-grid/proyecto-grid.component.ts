33834327c6eb2b29e2b3ee06826f8e25b08a9d71
Bernardo Veht <bveht@lizardcode.com.ar>
2022-05-26 22:59:00 -0300

    correccion de CORS

--- src/app/pages/mi-proyecto/components/proyecto-grid/proyecto-grid.component.ts
+++ src/app/pages/mi-proyecto/components/proyecto-grid/proyecto-grid.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Projects } from 'src/app/models/projects.model';
 import { AppState } from 'src/app/store/app.reducers';
 
 @Component({
@@ -11,11 +12,25 @@ export class ProyectoGridComponent implements OnInit {
   
   @Output() public readonly changeDetailMode = new EventEmitter<any>();
   constructor(private readonly store:Store<AppState>,) { }
+  
+  public datasource!: Projects[];
+  public headelements:string [] = [
+    'Código de la obra',
+    'Obra',
+    'Cantidad de rendiciones',
+    'Acumulado desembolsos',
+    'Acumulado Rendiciones',
+    'Monto de obra',
+    'Acciones'
+  ];
+ 
 
   ngOnInit(): void {
 
     this.store.select('projects').subscribe((state)=>{
-      console.log(state.projects);
+      if(state.projects){
+        this.datasource = state.projects;
+      }
     });
 
   }
