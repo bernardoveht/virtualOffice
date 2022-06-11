12c87fd62db841253054ffba1b720b40f850a4c3
Bernardo Veht <bveht@lizardcode.com.ar>
2022-06-11 13:20:30 -0300

    correccion de buscador

--- src/app/pages/mi-proyecto/components/proyecto-search/proyecto-search.component.ts
+++ src/app/pages/mi-proyecto/components/proyecto-search/proyecto-search.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { ProjectsFilter } from 'src/app/models/projects.model';
+import { AppState } from 'src/app/store/app.reducers';
+import * as projectActions from 'src/app/store/actions/projects/projects.actions'
 
 @Component({
   selector: 'app-proyecto-search',
@@ -7,9 +12,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProyectoSearchComponent implements OnInit {
 
-  openSearch = false;
+  public filter:ProjectsFilter = {
+    provinces: [],
+    page: 0,
+    pageSize: 50,
+    orderBy: '',
+    orderDescending: false,
+    id: '',
+    bapinCode: '',
+    name: '',
+    portfolioId: null,
+    departments: [],
+    municipalities: [],
+    localities: [],
+    workTypeGroupId: null,
+    workTypeSubgroupId: null,
+    workTypeId: null,
+    workflowSteps: [],
+    budgetaryProgramId: '',
+    tematicAreaId: null,
+    planIds: [],
+    tagValues: [],
+    managerUserId: '',
+    organismInChargeId: '',
+    beneficiaryOrganismId: '',
+    externalCreditOrganisms: [],
+    loanId: '',
+    developmentState: null,
+    workflowStepStatuses: [],
+    haveLocations: true,
+    isEmergency: false,
+    lastUpdateTo: '',
+    geoJsonLayerId: '',
+    geoJsonFeatureCollection: '',
+    includeArchived: false,
+    lastUpdateFrom: ''
+  };
 
-  constructor() { }
+  public openSearch = false;
+  public searchForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private readonly store: Store<AppState>,
+    ) {
+      this.searchForm = this.fb.group({
+        sippe:[''],
+        organismo:[''],
+        name:[''],
+        plan:[''],
+        fechaPresentacion:[''],
+  
+      })
+     }
 
   ngOnInit(): void {
   }
@@ -17,5 +72,12 @@ export class ProyectoSearchComponent implements OnInit {
   openSearcher(){
     this.openSearch = !this.openSearch;
   }
-
+  handleSearch(){
+    const {sippe,name} = this.searchForm.value;
+    this.filter.name = name ? name:'';
+    this.filter.id = sippe ? sippe:'';
+    this.store.dispatch(projectActions.getSearchProjects({filters:this.filter}));
+    this.openSearcher();
+    this.searchForm.reset();
+  }
 }
